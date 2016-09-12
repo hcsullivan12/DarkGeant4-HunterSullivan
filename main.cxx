1afcdef81b176349d0a37c810baae16a71c0cf91
Emma Davenport <Davenport.physics@gmail.com>
2016-09-12 10:19:45 -0500

    Added the ability to execute arbitrary mac files. Refactoring might be in order

--- main.cxx
+++ main.cxx
@@ -26,7 +26,6 @@
 #include "G4ThreeVector.hh"
 #include "G4VUserDetectorConstruction.hh"
 
-
 #ifdef G4VIS_USE
 
 	#include "G4VisExecutive.hh"
@@ -45,11 +44,25 @@
 #include "PhysicsList.hh"
 #include "PrimaryGeneratorAction.hh"
 
+// C & C++ Headers
+#include <cstring>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+using std::cout;
+
+static vector<G4String> ExecutionVector;
 
 //Function Prototypes
+void HandleArguments(int argc, char *argv[]);
 void InitializeRunManager(G4RunManager *runManager);
 
-int main(void) {
+int main(int argc, char *argv[]) {
+	
+	if (argc > 1)
+		HandleArguments(argc, argv);
 	
 	G4RunManager *runManager = new G4RunManager();
 	InitializeRunManager(runManager);
@@ -63,6 +76,8 @@ int main(void) {
 	InitializeUIManager(ui);
 #endif
 
+	std::cin.get();
+	runManager->BeamOn(30);
 	std::cin.get();
 
 #ifdef G4UI_USE
@@ -92,6 +107,9 @@ void InitializeUIManager(G4UImanager *ui) {
 	ui->ApplyCommand("/tracking/verbose 1");
 	
 	ui->ApplyCommand("/control/execute vis.mac");
+	
+	for (size_t i = 0; i < ExecutionVector.size();i++)
+		ui->ApplyCommand(ExecutionVector[i]);
 }
 #endif
 
@@ -102,4 +120,51 @@ void InitializeVisManager(G4VisManager *vis) {
 	
 }
 #endif
+
+// String-Function structure
+typedef struct {
+	
+	string name;
+	void (*Function)(int argc, char *argv[], int index);
+	
+} ArgumentTable;
+
+
+//Argument Function Prototypes
+void Execute_Argument(int argc, char *argv[], int index);
+
+
+//Argument Function table
+static const int numHandledArguments = 1;
+static const ArgumentTable Table[numHandledArguments] =
+{{"-execute", &Execute_Argument}};
+
+/*
+ * 
+ * Checks each provided runtime argument provided by the user
+ * against known runtime arguments and acts accordingly.
+ * 
+ * */
+void HandleArguments(int argc, char *argv[]) {
+	
+	for (int y = 1;y < argc;y++)
+		for (int x = 0;x < numHandledArguments;x++)
+			if (strcasecmp(argv[y], Table[x].name.c_str()) == 0)
+				Table[x].Function(argc, argv, y);		
+		
+}
+
+void Execute_Argument(int argc, char *argv[], int index) {
+
+	if ( (index+1) == argc) {
+	
+		cout << "Did not give a good execution argument";
+		return;
+		
+	}
+	
+	G4String CharLiteralToString(argv[index + 1]);
+	ExecutionVector.push_back("/control/execute " + CharLiteralToString);
+	
+}
 
