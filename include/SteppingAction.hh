e488977f5e4225e86f127470d6b1bb6d92135b16
Emma Davenport <Davenport.physics@gmail.com>
2016-10-15 14:30:16 -0500

    New features

---
+++ include/SteppingAction.hh
@@ -0,0 +1,71 @@
+/*
+ * SteppingAction.hh
+ * 
+ * Copyright 2016 Emma Davenport <Davenport.physics@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#ifndef STEPPINGACTION_H
+#define STEPPINGACTION_H
+
+
+// Geant4 Headers
+#include "G4Step.hh"
+#include "G4UserSteppingAction.hh"
+
+// C/C++ Headers
+#include <cstdio>
+
+
+
+class SteppingAction : public G4UserSteppingAction {
+	
+	
+	/*
+	 * Class Member Variables
+	 * 
+	 * */
+	 
+	private:
+		
+		FILE *fp;
+	
+	/*
+	 * Class Member Functions
+	 * 
+	 * */
+	 
+	public:
+	
+		SteppingAction();
+		~SteppingAction();
+		
+		void UserSteppingAction(const G4Step *Step);
+		
+	private:
+	
+		void SaveBanner(const G4Step *Step, G4Track *Track);
+		void SaveFormat();
+		void SaveStepData(const G4Step *Step, G4Track *Track);
+	
+};
+
+#endif
+
