c25160bbf91c11cc50576d0736cd47635b2490e7
Emma Davenport <Davenport.physics@gmail.com>
2016-10-27 08:22:03 -0500

    Modified JB module

---
+++ config/module/JB/Config.lua
@@ -0,0 +1,37 @@
+--[[
+
+Config
+~~~~~~~~~~~~~~~~~~
+
+	* Module_Name
+
+	* PhysicsList
+
+		1."Default"
+		2."QGSP_BERT"
+		3. ... 
+
+	* <Other parameters>
+
+]]--
+
+ConfigTable = { Module_Name = "JB Simple Model",
+                PhysicsList = "QGSP_BERT" }
+
+--[[
+
+File
+~~~~~~~~~~~~~~~~~~
+
+	* File specifies a file to be loaded. It must give the entire path from the
+	executable's standpoint
+
+	* Type specifies the type of data stored in the file given.
+
+		1. Fourvector ( E, P_x, P_y, P_z)
+		2. ...
+
+]]--
+
+FileTable = { File = nil,
+              Type = nil }
