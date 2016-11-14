95e0129b4395618d069250908cf2d4161502c837
Emma Davenport <Davenport.physics@gmail.com>
2016-11-14 09:02:41 -0600

    Removed unused code

--- config/Config.lua
+++ config/Config.lua
@@ -16,22 +16,5 @@ Config
 ]]--
 
 ConfigTable = { Module_Name = "Default",
-                PhysicsList = "Default" }
+                PhysicsList = "Default"}
 
---[[
-
-File
-~~~~~~~~~~~~~~~~~~
-
-	* File specifies a file to be loaded. It must give the entire path from the
-	executable's standpoint
-
-	* Type specifies the type of data stored in the file given.
-
-		1. Fourvector ( E, P_x, P_y, P_z)
-		2. ...
-
-]]--
-
-FileTable = { File = nil,
-              Type = nil }
