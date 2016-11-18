2dbcb3a87e675fa23d6d86453b6b971f2c408f54
hcsullivan12 <hcsullivan12@yahoo.com>
2016-11-18 00:26:34 -0600

    Last one, now I'll work on .cpp :)

---
+++ include/DetectorConstruction/DetectorComponentEllipticalCone.hh
@@ -0,0 +1,66 @@
+/*
+ * DetectorComponentEllipticalCone
+ * 
+ * Copyright 2016 Hunter Sullivan <hunter.sullivan@mavs.uta.edu>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#ifndef DETECTORCOMPONENTELLIPTICALCONE_HH
+#define DETECTORCOMPONENTELLIPTICALCONE_HH
+
+#include "DetectorComponent.hh"
+
+class DetectorComponent_EllipticalCone : public DetectorComponent {
+	
+	/*
+	 * 
+	 * Class member variables
+	 * 
+	 * */
+
+	public:
+	
+		G4double xSemiAxis;
+		G4double ySemiAxis;
+		G4double Height;
+		G4double zTop;
+		
+	/*
+	 * 
+	 * Class member functions
+	 * 
+	 * */
+
+	public:
+	
+		DetectorComponent_EllipticalCone(G4String Name,
+                                   G4double xSemiAxis,
+                                   G4double ySemiAxis,
+                                   G4double Height,
+                                   G4double zTop,
+                                   G4ThreeVector Position,
+                                   G4String MaterialString,
+                                   G4String Inside);
+		~DetectorComponent_EllipticalCone();
+	
+		void ConstructVolume();
+	
+};
+
+#endif
