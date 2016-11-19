b271f1f79a4f687a67bce650c86188b8d300cf8a
Emma Davenport <Davenport.physics@gmail.com>
2016-11-19 07:23:35 -0600

    Fixed units

--- scripts/DarkGeant4HelperScripts/StoppingPowerVsResidualRange/__main__.py
+++ scripts/DarkGeant4HelperScripts/StoppingPowerVsResidualRange/__main__.py
@@ -183,10 +183,10 @@ def GetResidualRangeList(Position):
 			#print("Event %d results in no length" % (idx+1))
 			continue
 			
-		TotalTrackLength = float(chunk[-1][4])
+		TotalTrackLength = float(chunk[-1][4])/10.0
 		
 		for i in range(0, len(chunk)):
-			ResidualRangeChunk.append(TotalTrackLength - float(chunk[i][4]))
+			ResidualRangeChunk.append(TotalTrackLength - float(chunk[i][4])/10.0)
 			
 		ResidualRange.append(list(ResidualRangeChunk))
 		ResidualRangeChunk = []
