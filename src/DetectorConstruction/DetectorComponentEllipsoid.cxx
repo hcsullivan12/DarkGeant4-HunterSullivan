d1275a9d6b0a7f748137d48c6651b186333fd4c3
hcsullivan12 <hcsullivan12@yahoo.com>
2016-11-18 22:55:10 -0600

    src code files for 3 more geometries

---
+++ src/DetectorConstruction/DetectorComponentEllipsoid.cxx
@@ -0,0 +1,72 @@
+/*
+ * DetectorComponentEllipsoid.cxx
+ * 
+ * Copyright 2016 Hunter Sullivan <hunter.sullivan@mavs.uta.edu>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include "DetectorComponentEllipsoid.hh"
+
+//Geant4 Headers
+#include "G4SystemOfUnits.hh"
+#include "G4Tubs.hh"
+#include "G4LogicalVolume.hh"
+
+DetectorComponent_Ellipsoid::DetectorComponent_Ellipsoid(
+				G4String Name,
+                                G4double xSemiAxis,
+                                G4double ySemiAxis,
+                                G4double zSemiAxis,
+                                G4double zBottom,
+                                G4double zTop,
+                                G4ThreeVector Position,
+                                G4String MaterialString,
+                                G4String Inside)
+ : DetectorComponent(Name, Ellipsoid, Position, MaterialString, Inside)
+{
+	
+	this->xSemiAxis = xSemiAxis;
+	this->ySemiAxis = ySemiAxis;
+	this->zSemiAxis = zSemiAxis;
+	this->zBottom = zBottom;
+	this->zTop = zTop;
+	
+}
+
+DetectorComponent_Ellipsoid::~DetectorComponent_Ellipsoid() {
+	
+	
+}
+
+void DetectorComponent_Ellipsoid::ConstructVolume() {
+	
+	G4Tubs *VirtualVolume = new G4Tubs(this->Name,
+                                       this->xSemiAxis * m,
+                                       this->ySemiAxis * m,
+                                       this->zSemiAxis * m,
+                                       this->zBottom * m,
+                                       this->zTop * m);
+	
+	this->LogicalVolume = new G4LogicalVolume(VirtualVolume,
+                 this->DetectorComponentMaterial->GetMaterialPointer(),
+                 this->Name);
+
+}
+
---
+++ src/DetectorConstruction/DetectorComponentEllipticalCone.cxx
@@ -0,0 +1,69 @@
+/*
+ * DetectorComponentEllipticalCone.cxx
+ * 
+ * Copyright 2016 Hunter Sullivan <hunter.sullivan@mavs.uta.edu>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include "DetectorComponentEllipticalCone.hh"
+
+//Geant4 Headers
+#include "G4SystemOfUnits.hh"
+#include "G4Tubs.hh"
+#include "G4LogicalVolume.hh"
+
+DetectorComponent_EllipticalCone::DetectorComponent_EllipticalCone(
+				G4String Name,
+                                G4double xSemiAxis;
+				G4double ySemiAxis;
+				G4double Height;
+				G4double zTop;
+				G4ThreeVector Position,
+                                G4String MaterialString,
+                                G4String Inside)
+ : DetectorComponent(Name, EllipticalCone, Position, MaterialString, Inside)
+{
+	
+	this->xSemiAxis = xSemiAxis;
+	this->ySemiAxis = ySemiAxis;
+	this->Height = Height;
+	this->zTop = zTop;
+	
+}
+
+DetectorComponent_EllipticalCone::~DetectorComponent_EllipticalCone() {
+	
+	
+}
+
+void DetectorComponent_EllipticalCone::ConstructVolume() {
+	
+	G4Tubs *VirtualVolume = new G4Tubs(this->Name,
+					this->xSemiAxis * m,
+                                        this->ySemiAxis * m,
+                                        this->Height * m,
+                                        this->zTop * m);
+	
+	this->LogicalVolume = new G4LogicalVolume(VirtualVolume,
+		 this->DetectorComponentMaterial->GetMaterialPointer(),
+                 this->Name);
+
+}
+
