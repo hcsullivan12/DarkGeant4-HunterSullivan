4ddc652484638b9bdbf653969175069161c19d2d
hcsullivan12 <hcsullivan12@yahoo.com>
2016-12-19 17:07:45 -0600

    Documentation for Components

--- config/DetectorConfig.lua
+++ config/DetectorConfig.lua
@@ -31,15 +31,109 @@ DetectorConfig = { Detector_Name = "Detector",
 		
 			Inner_Radius (Default value of 0.0)
 			Outer_Radius (No Default Value)
-			Half_Length (No Default Value)
 			Start_Angle (Default Value of 0.0)
 			Delta_Angle   (Default Value of 360.)
+			Half_Length (No Default Value)
 		
 		"Box"
 		
 			X (No Default Value) (Width) -> Usually
 			Y (No Default Value) (Height)-> Usually
 			Z (No Default Value) (Length)-> Usually
+
+
+		"Cone"
+
+			Inner_Radius_At_Bottom (Default value of 0.0)
+			Outside_Radius_At_Bottom (No Default Value)
+			Inner_Radius_At_Top (Default value of 0.0)
+			Outside_Radius_At_Top (No Default Value)
+			Half_Length (No Default Value)
+			Start_Angle (Default value of 0.0)
+			Delta_Angle (Default value of 360.)
+
+		"Ellipsoid"
+
+			X_Semi_Axis (No Default Value)
+			Y_Semi_Axis (No Default Value)
+			Z_Semi_Axis (No Default Value)
+			Z_Bottom (No Default Value)
+			Z_Top (No Default Value)
+
+		"Elliptical Cone"
+
+			X_Semi_Axis (No Default Value)
+			Y_Semi_Axis (No Default Value)
+			Height (No Default Value)
+			Z_Top (No Default Value)
+		
+		"Elliptical Tube"
+
+			X_Half_Length (No Default Value)
+			Y_Half_Length (No Default Value)
+			Z_Half_Length (No Default Value)			
+		
+		"Hyperbolic Tube"
+
+			Inner_Radius (No Default Value)
+			Outer_Radius (No Default Value)
+			Inner_Radius_Angle (No Default Value)
+			Outer_Radius_Angle (No Default Value)
+			Z_Half_Length (No Default Value)
+
+		"Parallelepiped"
+
+			X_Half_Length (No Default Value)
+			Y_Half_Length (No Default Value) 
+			Z_Half_Length (No Default Value) 
+			Angle_Of_XZ_Faces (No Default Value)
+			Polar_Angle_Of_XY_Faces (No Default Value)
+			Azimuthal_Angle_Of_XY_Faces (No Default Value)
+
+		"Solid Sphere"
+
+			Radius (No Default Value)
+		
+		"Spherical Shell"
+		
+			Inner_Radius (Default value of 0.0)
+			Outer_Radius (No Default Value)
+			Phi_Start (Default value of 0.0)
+			Delta_Phi (Default value of 360.)
+			Theta_Start (Default value of 0.0)
+			Delta_Theta (Default value of 360.)
+
+		"Torus"
+
+			Inner_Radius (Default value of 0.0)
+			Outer_Radius (No Default Value)
+			Sweeping_Radius (No Default Value)
+			Phi_Start (Default value of 0.0) 
+			Delta_Phi (Default value of 360.)
+
+		"Trapezoid"
+
+			X_Half_Length_At_Bottom (No Default Value)
+			X_Half_Length_At_Top (No Default Value)
+			Y_Half_Length_At_Bottom (No Default Value)
+			Y_Half_Length_At_Top (No Default Value)
+			Z_Half_Length (No Default Value)
+
+		"Twisted Box"
+
+			Twisting_Angle (No Default Value)
+			X_Half_Length (No Default Value)
+			Y_Half_Length (No Default Value)
+			Z_Half_Length (No Default Value)
+
+		"ZTwistedTrapezoid"
+
+			X_Half_Length_At_Bottom (No Default Value)
+			X_Half_Length_At_Top (No Default Value)
+			Y_Half_Length_At_Bottom (No Default Value)
+			Y_Half_Length_At_Top (No Default Value)
+			Z_Half_Length (No Default Value)
+			Twisting_Angle (Default value of 0.0)
 
 ]]--
 World = { Material = "G4_AIR",
@@ -76,6 +170,99 @@ World = { Material = "G4_AIR",
 			X (No Default Value) (Width) -> Usually
 			Y (No Default Value) (Height)-> Usually
 			Z (No Default Value) (Length)-> Usually
+
+		"Cone"
+
+			Inner_Radius_At_Bottom (Default value of 0.0)
+			Outside_Radius_At_Bottom (No Default Value)
+			Inner_Radius_At_Top (Default value of 0.0)
+			Outside_Radius_At_Top (No Default Value)
+			Half_Length (No Default Value)
+			Start_Angle (Default value of 0.0)
+			Delta_Angle (Default value of 360.)
+
+		"Ellipsoid"
+
+			X_Semi_Axis (No Default Value)
+			Y_Semi_Axis (No Default Value)
+			Z_Semi_Axis (No Default Value)
+			Z_Bottom (No Default Value)
+			Z_Top (No Default Value)
+
+		"Elliptical Cone"
+
+			X_Semi_Axis (No Default Value)
+			Y_Semi_Axis (No Default Value)
+			Height (No Default Value)
+			Z_Top (No Default Value)
+		
+		"Elliptical Tube"
+
+			X_Half_Length (No Default Value)
+			Y_Half_Length (No Default Value)
+			Z_Half_Length (No Default Value)			
+		
+		"Hyperbolic Tube"
+
+			Inner_Radius (No Default Value)
+			Outer_Radius (No Default Value)
+			Inner_Radius_Angle (No Default Value)
+			Outer_Radius_Angle (No Default Value)
+			Z_Half_Length (No Default Value)
+
+		"Parallelepiped"
+
+			X_Half_Length (No Default Value)
+			Y_Half_Length (No Default Value) 
+			Z_Half_Length (No Default Value) 
+			Angle_Of_XZ_Faces (No Default Value)
+			Polar_Angle_Of_XY_Faces (No Default Value)
+			Azimuthal_Angle_Of_XY_Faces (No Default Value)
+
+		"Solid Sphere"
+
+			Radius (No Default Value)
+		
+		"Spherical Shell"
+		
+			Inner_Radius (Default value of 0.0)
+			Outer_Radius (No Default Value)
+			Phi_Start (Default value of 0.0)
+			Delta_Phi (Default value of 360.)
+			Theta_Start (Default value of 0.0)
+			Delta_Theta (Default value of 360.)
+
+		"Torus"
+
+			Inner_Radius (Default value of 0.0)
+			Outer_Radius (No Default Value)
+			Sweeping_Radius (No Default Value)
+			Phi_Start (Default value of 0.0) 
+			Delta_Phi (Default value of 360.)
+
+		"Trapezoid"
+
+			X_Half_Length_At_Bottom (No Default Value)
+			X_Half_Length_At_Top (No Default Value)
+			Y_Half_Length_At_Bottom (No Default Value)
+			Y_Half_Length_At_Top (No Default Value)
+			Z_Half_Length (No Default Value)
+
+		"Twisted Box"
+
+			Twisting_Angle (No Default Value)
+			X_Half_Length (No Default Value)
+			Y_Half_Length (No Default Value)
+			Z_Half_Length (No Default Value)
+
+		"ZTwistedTrapezoid"
+
+			X_Half_Length_At_Bottom (No Default Value)
+			X_Half_Length_At_Top (No Default Value)
+			Y_Half_Length_At_Bottom (No Default Value)
+			Y_Half_Length_At_Top (No Default Value)
+			Z_Half_Length (No Default Value)
+			Twisting_Angle (Default value of 0.0)
 			
 	* X_Rotation
 	
