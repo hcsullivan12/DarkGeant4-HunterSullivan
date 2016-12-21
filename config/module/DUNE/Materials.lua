9a1f3383533e9449d9914817aa3fbe525bf4fa8b
Emma Davenport <Davenport.physics@gmail.com>
2016-12-21 15:36:39 -0600

    Removed redundant documentation

--- config/module/DUNE/Materials.lua
+++ config/module/DUNE/Materials.lua
@@ -1,59 +1,8 @@
 
-
---[[
-
-	* Number_Of_Materials 
-	
-		Must always be defined and must be an integer
-
-]]--
 Number_Of_Materials = 2
 
---[[
-
-	* Name
-	
-		Name of your material. Case sensitive, will be used later for
-		detector components! Be consistent.
-		
-	* G4Database
-	
-		true if you want DarkGeant4 to use the Geant4 Databases to
-		generate a material for you.
-		
-		default = false/nil and doesn't need to be defined.
-
-		* G4Name
-	
-			If G4Database is set to true, please be sure to specify what
-			material in the Geant4 Database your material refers to.
-		
-			default = nil. Will halt execution if not defined.
-			
-	If G4Database = false/nil
-	
-	* Number_Of_Protons
-	
-		Be sure to pass an integer! A fraction of a proton certainly
-		doesn't make sense.
-		
-		default = nil. Will halt execution if not defined.
-		
-	* Atomic_Mass (g/mole)
-	
-		default = nil. Will halt execution if not defined.
-		
-	* Density (g/cm^3)
-	
-		default = nil. Will halt execution if not defined.
-		
-	
-		
-	
-]]--
 Material_1 = { G4Database = true,
               G4Name = "G4_AIR"  }
-              
               
 Material_2 = { Name = "Liquid Argon",
                Number_Of_Protons = 18,
--- config/module/DUNE/Particles.lua
+++ config/module/DUNE/Particles.lua
@@ -58,72 +58,8 @@ function BoxMullerTransform()
 
 end
 
-
-
---[[
-
---]]
 Particle_File = "output.dat"
-
---[[
-
-	Particle_File_Type
-	
-	* Values
-	
-		"Four Vector"
-		
-			Standard format of a four vector file
-			<E/c, P_x, P_y, P_z> where each component is in units of
-			GeV/c
-		
-		"Four Vector, with name"
-		
-			Name, <E/c, P_x, P_y, P_z> 
-			where Name is the name of the particle being described
-		
-		"Four Vector, with name, with position"
-			
-			Name, <E/c, P_x, P_y, P_z>, <X, Y, Z>
-			where <X, Y, Z> define the position of the particle with
-			respect to the origin of the world box, with units of
-			metres.
-		
-		"Four Vector, with position, with name"
-		
-			Same as the previous version
-		
-		"Four Vector, with position"
-		
-			<E/c, P_x, P_y, P_z>, <X, Y, Z>
-	
-		default value = nil.
-
---]]
 Particle_File_Type = "Four Vector"
-
---[[
-
-	Four_Vector_Table
-	
-	* Values
-	
-		Particle_Name
-		
-			May be defined as any known particle
-			
-			default value = nil. Will halt execution if "with name" was
-			not a component of Particle_File_Type
-			
-		Particles_Position
-		
-			Must be defined as a table with three numerical quantities
-			<X, Y, Z>
-			
-			default = nil. Will halt execution if "with position" was
-			not a component of Particle_File_Type
-
---]]
 Particle_Table = { Particle_Name = "proton",
                    Particles_Position = Particle_Position_Function }
 
