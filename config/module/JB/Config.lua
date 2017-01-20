2a9706358720ea6d290121b30ba15d4c8f4c4a1c
Emma Davenport <Davenport.physics@gmail.com>
2017-01-20 03:48:10 -0600

    Removed old JB module

--- config/module/JB/Config.lua
+++
@@ -1,5 +0,0 @@
-ConfigTable = { Module_Name = "Default",
-                PhysicsList = "QGSP_BERT" }
-
-DarkGeantOutputFile = { Name = "DarkGeantOutput.dat",
-                        Location = nil }
--- config/module/JB/Materials.lua
+++
@@ -1,9 +0,0 @@
-Number_Of_Materials = 2
-
-Material_1 = { G4Database = true,
-              G4Name = "G4_AIR"  }
-                            
-Material_2 = { Name = "Liquid Argon",
-               Number_Of_Protons = 18,
-               Atomic_Mass = 39.95,
-               Density = 1.390 }
