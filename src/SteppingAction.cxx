f2413baffc05ee1ccd6239af7fe97223c610eb7a
hcsullivan12 <hcsullivan12@yahoo.com>
2017-03-17 12:06:35 -0500

    Fixed the precision on Output file.

--- src/SteppingAction.cxx
+++ src/SteppingAction.cxx
@@ -138,13 +138,13 @@ void SteppingAction::SaveBanner(const G4Step *Step, G4Track *Track) {
 void SteppingAction::SaveFormat() {
 	
 	fprintf(this->fp,"%-5s","Step#");
-	fprintf(this->fp,"%15s","X(mm)");
-	fprintf(this->fp,"%15s","Y(mm)");
-	fprintf(this->fp,"%15s","Z(mm)");
-	fprintf(this->fp,"%15s","KinE(MeV)");
-	fprintf(this->fp,"%15s","dE(MeV)");
-	fprintf(this->fp,"%15s","StepLeng");
-	fprintf(this->fp,"%15s","TrackLeng");
+	fprintf(this->fp,"%25s","X(mm)");
+	fprintf(this->fp,"%25s","Y(mm)");
+	fprintf(this->fp,"%25s","Z(mm)");
+	fprintf(this->fp,"%25s","KinE(MeV)");
+	fprintf(this->fp,"%25s","dE(MeV)");
+	fprintf(this->fp,"%25s","StepLeng");
+	fprintf(this->fp,"%25s","TrackLeng");
 	fprintf(this->fp,"%20s","NextVolume");
 	fprintf(this->fp,"%20s","ProcName\n");
 	
@@ -176,13 +176,13 @@ void SteppingAction::Save0StepData(const G4Step *Step) {
 	G4StepPoint *PreStepPoint = Step->GetPreStepPoint();
 	
 	fprintf(this->fp,"%-5s","0");
-	fprintf(this->fp,"%15.5f", PreStepPoint->GetPosition().x());
-	fprintf(this->fp,"%15.5f", PreStepPoint->GetPosition().y());
-	fprintf(this->fp,"%15.5f", PreStepPoint->GetPosition().z());
-	fprintf(this->fp,"%15.5f", PreStepPoint->GetKineticEnergy());
-	fprintf(this->fp,"%15s","0.00000");
-	fprintf(this->fp,"%15s","0.00000");
-	fprintf(this->fp,"%15s","0.00000");
+	fprintf(this->fp,"%25.15f", PreStepPoint->GetPosition().x());
+	fprintf(this->fp,"%25.15f", PreStepPoint->GetPosition().y());
+	fprintf(this->fp,"%25.15f", PreStepPoint->GetPosition().z());
+	fprintf(this->fp,"%25.15f", PreStepPoint->GetKineticEnergy());
+	fprintf(this->fp,"%25s","0.000000000000000");
+	fprintf(this->fp,"%25s","0.000000000000000");
+	fprintf(this->fp,"%25s","0.000000000000000");
 	fprintf(this->fp,"%20s","Detector");      
 	fprintf(this->fp,"%20s","InitStep\n");
 }
@@ -200,13 +200,13 @@ void SteppingAction::Save0StepData(const G4Step *Step) {
 void SteppingAction::SaveStepData(const G4Step *Step, G4Track *Track) {
 	
 	fprintf(this->fp,"%-5d", Track->GetCurrentStepNumber());
-	fprintf(this->fp, "%15.5f", Track->GetPosition().x());
-	fprintf(this->fp, "%15.5f", Track->GetPosition().y());
-	fprintf(this->fp, "%15.5f", Track->GetPosition().z());
-	fprintf(this->fp, "%15.5f", Track->GetKineticEnergy());
-	fprintf(this->fp, "%15.5f", Step->GetTotalEnergyDeposit());
-	fprintf(this->fp, "%15.5f", Step->GetStepLength());
-	fprintf(this->fp, "%15.5f", Track->GetTrackLength());
+	fprintf(this->fp, "%25.15f", Track->GetPosition().x());
+	fprintf(this->fp, "%25.15f", Track->GetPosition().y());
+	fprintf(this->fp, "%25.15f", Track->GetPosition().z());
+	fprintf(this->fp, "%25.15f", Track->GetKineticEnergy());
+	fprintf(this->fp, "%25.15f", Step->GetTotalEnergyDeposit());
+	fprintf(this->fp, "%25.15f", Step->GetStepLength());
+	fprintf(this->fp, "%25.15f", Track->GetTrackLength());
 	
 	/*
 	 * If the volume exists, save which volume the particle is presently
